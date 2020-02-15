6e2a4b69601f475b22f047d2980bdbe95724b151
Wouter Hemeryck <wouter.hemeryck@delawareconsulting.com>
2020-02-15 19:20:38 +0100

    Additional testing for feedingmanager

--- tests/JOHNNYbeGOOD.Home.Tests.UnitTests/Managers/FeedingManagerTests.cs
+++ tests/JOHNNYbeGOOD.Home.Tests.UnitTests/Managers/FeedingManagerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using JOHNNYbeGOOD.Home.Engines;
 using JOHNNYbeGOOD.Home.FeedingManager;
@@ -124,6 +125,75 @@ namespace JOHNNYbeGOOD.Home.Tests.UnitTests.Managers
 
             Assert.False(result.Succeeded);
             Assert.Null(result.SlotUsed);
+        }
+
+        [Fact]
+        public void FeedingFailsIfSlotRemainsClosed()
+        {
+            var mocker = new AutoMocker();
+            var sensor = mocker.GetMock<IDigitalSensor>();
+
+            var slot = new FeedingSlot("dummy",
+                mocker.Get<IGateDevice>(),
+                sensor.Object,
+                Enumerable.Empty<FeedingSlot>(),
+                mocker.Get<IThingsResource>());
+
+            sensor.Setup(s => s.Read())
+                .Returns(true)
+                .Verifiable();
+
+            var manager = new DefaultFeedingManager(new[] { slot },
+                mocker.Get<ILogger<DefaultFeedingManager>>(),
+                mocker.Get<ISchedulingEngine>());
+
+            var result = manager.TryFeed();
+
+            sensor.VerifyAll();
+
+            Assert.False(result.Succeeded);
+        }
+
+        [Theory]
+        [InlineData(new[] { true, true, true }, 0)]
+        [InlineData(new[] { false, true, true }, 1)]
+        [InlineData(new[] { true, false, true }, 0)]
+        [InlineData(new[] { false, false, true }, 2)]
+        [InlineData(new[] { false, false, false }, null)]
+        public void CorrectSlotTheory(bool[] slotsClosed, int? nextSlot)
+        {
+            var sensors = new List<IDigitalSensor>();
+            var slots = new List<FeedingSlot>();
+
+            for (int i = 0; i < slotsClosed.Length; i++)
+            {
+                var sensor = new Mock<IDigitalSensor>();
+                sensor.Setup(s => s.Read()).Returns(slotsClosed[i]).Verifiable();
+                sensors.Add(sensor.Object);
+
+                slots.Add(new FeedingSlot(i.ToString(),
+                    _mocker.Get<IGateDevice>(),
+                    sensor.Object,
+                    slots,
+                    _mocker.Get<IThingsResource>()));
+            }
+
+            var manager = new DefaultFeedingManager(slots,
+                _mocker.Get<ILogger<DefaultFeedingManager>>(),
+                _mocker.Get<ISchedulingEngine>());
+
+            var result = manager.NextFeedingSlot();
+
+            _mocker.VerifyAll();
+
+            if (nextSlot.HasValue)
+            {
+                Assert.Equal(nextSlot.Value.ToString(), result.Name);
+            }
+            else
+            {
+                Assert.Null(result);
+            }
         }
     }
 }
